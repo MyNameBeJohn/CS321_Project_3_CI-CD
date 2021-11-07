0c3eba1dc2ec2402c1c99bdc8d928ab480761ca0
dhruvjoshi1998 <dhruvjoshi1998@gmail.com>
2021-11-07 12:10:32 -0500

    working base app

--- Project3/app.py
+++ Project3/app.py
@@ -1,25 +1,10 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-visitors = []
-
 @app.route("/")
-@app.route("/welcome/")
 def welcome():
-	user = {"username": ""}
-	return render_template("base.html",
-							title="welcome",
-							user=user,
-							visitors=visitors)
+	return render_template("base.html")
 
-@app.route("/app")
-def to_do():
-	return render_template("base_todo.html",
-							title=f"{user} To Do List")
-
-@app.route("/add", methods=["POST"])
-def add():
 
 if __name__ == "__main__":
 	app.run()
