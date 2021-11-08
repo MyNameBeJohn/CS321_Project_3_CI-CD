8c964a3943610828001013c15928fc438511fbd9
dhruvjoshi1998 <dhruvjoshi1998@gmail.com>
2021-11-07 22:38:33 -0500

    restructured project

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/welcome/")
 def welcome():
-	return render_template("base.html")
-
+	return    render_template("base.html")
 
 if __name__ == "__main__":
 	app.run()
