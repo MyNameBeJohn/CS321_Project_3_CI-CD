24bf6fe75fdc121081360ff22efae4131f4c4698
dhruvjoshi1998 <dhruvjoshi1998@gmail.com>
2021-11-08 14:47:14 -0500

    basic web app functionality

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+todo_list = []
+
 
 @app.route("/")
-@app.route("/welcome/")
-def welcome():
-    return render_template("base.html")
+def home():
+    return render_template("base.html", todo_list=todo_list)
+
+@app.route("/add/", methods=["POST"])
+def add():
+	new_todo_item = request.form.get("new_todo")
+	todo_list.append(new_todo_item)
+
+	return redirect(url_for("home"))
+
+
+@app.route("/remove/<string:task>")
+def remove(task):
+	todo_list.remove(task)
+	return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
--- test_app.py
+++ test_app.py
@@ -1,13 +1,64 @@
 from app import *
-import requests
+from flask import template_rendered
 
+# website_path = "https://to-do-cs321.herokuapp.com/"
+website_path = "http://127.0.0.1:5000/"
 
-website_path = "https://to-do-cs321.herokuapp.com/"
-#website_path = "http://127.0.0.1:5000/"
-
-def test_welcome():
+def test_home():
 	# more intreesting way to test the the page works
 	client = app.test_client()
 	response = client.get("/")
 	print(response.status_code)
 	assert response.status_code == 200  # success
+
+
+def test_add():
+	# add an item
+	client = app.test_client()
+	url = "/add/"
+	data = {"new_todo": "Task 1"}
+	response = client.post(url, data=data)
+
+	# make sure it redirects
+	assert response.status_code == 302
+
+	# make sure item is on home page
+	response = client.get("/")
+	webpage_text = response.get_data()
+	assert b"Task 1" in response.data
+
+	# add another item
+	data = {"new_todo": "Task 2"}
+	response = client.post(url, data=data)
+
+	# check list len
+	assert len(todo_list) == 2
+
+	# make sure both added items on home page
+	response = client.get("/")
+	webpage_text = response.get_data()
+	assert b"Task 1" in response.data
+	assert b"Task 2" in response.data
+
+
+def test_remove():
+	client = app.test_client()
+
+	# make sure item is on home page
+	response = client.get("/")
+	webpage_text = response.get_data()
+	assert b"Task 1" in response.data
+
+	# remove item
+	url = "/remove/Task 1"
+	response = client.get(url)
+
+	# make sure it redirects
+	assert response.status_code == 302
+
+	# make sure removed item not on home page
+	response = client.get("/")
+	webpage_text = response.get_data()
+	print(webpage_text)
+	assert b"Task 1" not in response.data
+
