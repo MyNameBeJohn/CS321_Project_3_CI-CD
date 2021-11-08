2a274b0d904e8cf10ff8837d4f31835388e50f3d
dhruvjoshi1998 <dhruvjoshi1998@gmail.com>
2021-11-07 22:41:04 -0500

    restructured project

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/welcome/")
 def welcome():
-	return    render_template("base.html")
+	return render_template("base.html")
 
 if __name__ == "__main__":
 	app.run()
